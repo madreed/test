caab01df41c9e550c114aba13c2465693460de06
matthew reed <madreed@ucsc.edu>
2019-05-08 17:25:18 -0700

    Added States

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 //Main.js for the git test
 //its ya boi Matty-kun
+
+"use strict";
+
+//Define globals here
+
+//Define Game
+var game = new Phaser.Game(1000, 1000, Phaser.AUTO);
+
+//starting game after the window loads
+
+window.onload = function(){
+	//defining our states
+	game.state.add('Load', Load);
+	game.state.add('Play', Play);
+	game.state.start("Load");
+}
